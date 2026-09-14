caaa1fc0ca3cb4c0065871e8c6beba2b74dc3432
i <i@github.com>
2026-09-14 11:12:17 +0800

    release: 26.914.1112

--- dist/nodeseek-plus-plus.meta.js
+++ dist/nodeseek-plus-plus.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NodeSeek++
 // @namespace    nodeseek-plus-plus
-// @version      26.914.1107
+// @version      26.914.1112
 // @description  模块化论坛增强：阅读、过滤、回复、签到、交易与关键词监控，一个功能一套实现。
 // @license      GPL-3.0-only
 // @match        https://www.nodeseek.com/*
